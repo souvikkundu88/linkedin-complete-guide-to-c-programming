0bf846838aa0e1092f78adb4856ca4c03d8f1945
Souvik Kundu <kivtech88@gmail.com>
2024-09-07 14:26:38 +0530

    Update 01_07-challenge1.c

--- CH01/01_07/01_07-challenge1.c
+++ CH01/01_07/01_07-challenge1.c
@@ -4,13 +4,16 @@ int main()
 {
 	int a,b;
 
-	printf("Type a positive value: );
+	printf("Type a positive value: ");
 	scanf("%d",&b);
 	for(a=0;a<b;a++)
 	{
 		printf("I must do this %d times\n",b);
-		if(a==9)
-			break
+		{
+			if (a == 9)
+				break
+		}
+		
 	}
 
 	return(0);
